3111bbf034aa351d4788256ce2f028976bd15c80
Tony810-mã <quangvu37781@gmail.com>
2024-12-29 17:19:11 +0700

    feat: responsive detail blog

--- src/views/DetailBlog/ContentDetailBlog.vue
+++ src/views/DetailBlog/ContentDetailBlog.vue
@@ -10,23 +10,33 @@ const blog = JSON.parse(localStorage.getItem("blogs"));
 const filterBlog = blog.find((blog) => Number(blog.id) === Number(idBlog));
 </script>
 <template>
-  <div class="space-y-4">
-    <div class="flex justify-between items-center">
-      <span v-if="filterBlog.dateCreate"
-        >Date created
-        <strong>
-          {{ format(new Date(filterBlog.dateCreate), "dd-MM-yyyy") }}</strong
-        ></span
-      >
-      <span class="font-sans text-xl block font-bold uppercase">{{
-        filterBlog?.title
-      }}</span>
-      <span v-if="filterBlog.dateUpdate"
-        >Last updated
-        <strong>
-          {{ format(new Date(filterBlog.dateUpdate), "dd-MM-yyyy") }}</strong
-        ></span
-      >
+  <div class="space-y-4 px-2">
+    <span class="font-sans text-xl text-center block font-bold uppercase">{{
+      filterBlog?.title
+    }}</span>
+
+    <div class="flex flex-col gap-2">
+      <span class="font-sans text-base">
+        By <strong>{{ filterBlog?.author }}</strong>
+      </span>
+      <div class="flex flex-col sm:flex-row gap-2 sm:items-center">
+        <span v-if="filterBlog.dateCreate" class="font-sans text-xs sm:text-sm"
+          >Created
+          <strong>
+            {{
+              format(new Date(filterBlog.dateCreate), "dd MMMM, yyyy")
+            }}</strong
+          ></span
+        >
+        <span v-if="filterBlog.dateUpdate" class="font-sans text-xs sm:text-sm"
+          ><span class="hidden sm:inline-block">•</span> Updated
+          <strong>
+            {{
+              format(new Date(filterBlog.dateUpdate), "dd MMMM, yyyy")
+            }}</strong
+          ></span
+        >
+      </div>
     </div>
     <p v-html="filterBlog?.content"></p>
   </div>
